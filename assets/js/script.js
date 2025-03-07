e59683dd93a6866e074eaff22d28083a928067b8
Pati <patrycja.fixl@icloud.com>
2025-03-07 19:41:29 +0000

    07032025

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,234 @@
 // filepath: /Users/patrycjafixl/Library/Mobile Documents/com~apple~CloudDocs/Documents/GDY4/Digital Publishing /DP_Digital-Publishing_Thesis/thesis-website-github/boilerplate-without-css/assets/js/script.js
+
+function docReady(fn) {
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn); // Calls function when DOM is fully loaded
+  }
+}
+
+docReady(function () {
+  var dictionaryToggleBtn = document.getElementById("toggle-btn");
+
+  if (dictionaryToggleBtn) {
+    dictionaryToggleBtn.addEventListener("click", toggleDictionary);
+  } else {
+    console.error("Button not found: #toggle-btn");
+  }
+
+  function toggleDictionary() {
+    var terms = document.getElementById("terms");
+
+    if (terms.classList.contains("active")) {
+      // disable all accordions
+      const activeAccordions = terms.querySelectorAll(".active.accordion");
+      for (const accordion of activeAccordions) {
+        accordion.classList.remove("active");
+        const panel = accordion.parentElement.querySelector(".panel");
+        if (panel) {
+          panel.style.maxHeight = "";
+        }
+      }
+
+      terms.classList.remove("transition");
+
+      setTimeout(function (e) {
+        terms.classList.remove("active");
+      }, 250);
+    } else {
+      terms.classList.add("active");
+
+      setTimeout(function (e) {
+        terms.classList.add("transition");
+      }, 1);
+    }
+  }
+
+  const abstractLink = document.querySelector('a[href="#abstract"]');
+  const introLink = document.querySelector('a[href="#intro"]');
+  const impulseLink = document.querySelector('a[href="#chapter-1"]');
+  const prepLink = document.querySelector('a[href="#chapter-2"]');
+  const travelLink = document.querySelector('a[href="#chapter-3"]');
+  const exploreLink = document.querySelector('a[href="#chapter-4"]');
+  const epilogueLink = document.querySelector('a[href="#epilogue"]');
+
+  // Get both the elements that need the blur effect removed
+  const abstractTitle = document.getElementById("abstract-title");
+  const abstractText = document.getElementById("abstract-text");
+  const introTitle = document.getElementById("intro-title");
+  const introText = document.getElementById("intro-text");
+  const impulseTitle = document.getElementById("chapter-1-title");
+  const impulseText = document.getElementById("chapter-1-text");
+  const prepTitle = document.getElementById("chapter-2-title");
+  const prepText = document.getElementById("chapter-2-text");
+  const travelTitle = document.getElementById("chapter-3-title");
+  const travelText = document.getElementById("chapter-3-text");
+  const exploreTitle = document.getElementById("chapter-4-title");
+  const exploreText = document.getElementById("chapter-4-text");
+  const epilogueTitle = document.getElementById("epilogue-title");
+  const epilogueText = document.getElementById("epilogue-text");
+
+  const unBlurSections = [
+    {
+      link: abstractLink,
+      title: abstractTitle,
+      text: abstractText,
+    },
+    {
+      link: introLink,
+      title: introTitle,
+      text: introText,
+    },
+    {
+      link: impulseLink,
+      title: impulseTitle,
+      text: impulseText,
+    },
+    {
+      link: prepLink,
+      title: prepTitle,
+      text: prepText,
+    },
+    {
+      link: travelLink,
+      title: travelTitle,
+      text: travelText,
+    },
+    {
+      link: exploreLink,
+      title: exploreTitle,
+      text: exploreText,
+    },
+    {
+      link: epilogueLink,
+      title: epilogueTitle,
+      text: epilogueText,
+    },
+  ];
+
+  function unBlurSection(event, title, text) {
+    event.preventDefault();
+    title.style.filter = "none";
+    text.style.filter = "none";
+
+    title.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+
+  unBlurSections.forEach((section) => {
+    section.link.addEventListener("click", function (event) {
+      unBlurSection(event, section.title, section.text);
+    });
+  });
+
+  const sections = document.querySelectorAll(
+    "main #abstract-title, main section.chapter"
+  );
+  const navLinks = document.querySelectorAll(".index a");
+  const circle = document.querySelector(".circle");
+
+  /**
+   * @type {HTMLElement}
+   */
+  const mainElement = document.querySelector("main");
+
+  function setActiveLink() {
+    let index = sections.length;
+
+    const scrollY =
+      window.innerWidth <= 768 ? window.scrollY : mainElement.scrollTop;
+    while (--index && scrollY + 100 < sections[index].offsetTop) {}
+
+    navLinks.forEach((link) => link.classList.remove("active"));
+    if (navLinks[index]) {
+      navLinks[index].classList.add("active");
+
+      const activeLink = navLinks[index];
+      const linkRect = activeLink.getBoundingClientRect();
+      const circleTop = linkRect.top;
+      const circleLeft = linkRect.left;
+      // console.log(activeLink);
+
+      if (window.innerWidth <= 768) {
+        // console.log('left updated');
+        console.log(circleLeft);
+
+        circle.style.left = `${circleLeft}px`;
+        circle.style.top = `${circleTop}px`;
+        circle.style.marginLeft = "5px";
+        circle.style.marginTop = "10px";
+      } else {
+        // console.log('top updated');
+        circle.style.top = `${circleTop}px`;
+        circle.style.left = "0px";
+        circle.style.marginLeft = "28px";
+        circle.style.marginTop = "10px";
+      }
+    }
+  }
+
+  setActiveLink();
+
+  window.addEventListener("scroll", setActiveLink);
+
+  mainElement.addEventListener("scroll", setActiveLink);
+
+  let thisActiveMenuItem = "";
+  let linkRect = "";
+  let circleTop = "";
+
+
+  window.addEventListener("resize", function () {
+    document.body.classList.add("resize-animation-stopper");
+    linkRect = thisActiveMenuItem.getBoundingClientRect();
+    circleTop = linkRect.top + window.scrollY;
+    circle.style.top = `${circleTop}px`;
+    setTimeout(function (e) {
+      document.body.classList.remove("resize-animation-stopper");
+    }, 250);
+  });
+
+  // window.addEventListener("scroll", unBlurOnView);
+  unBlurOnView(unBlurSections);
+});
+
+function unBlurOnView(targets) {
+  const options = {
+    rootMargin: "0px 0px -70%",
+    threshold: 0,
+  };
+
+  const intersectionCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.filter = "none";
+      } else {
+        entry.target.removeAttribute("style");
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(intersectionCallback, options);
+
+  // targets.forEach((target) => {
+  //   observer.observe(target.title);
+  //   observer.observe(target.text);
+  // });
+
+  document
+    .querySelectorAll(
+      "section.chapter h2, section.chapter p, section.chapter figure, section.chapter figcaption"
+    )
+    .forEach((target) => {
+      observer.observe(target);
+    });
+}
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -24,156 +254,8 @@ for (i = 0; i < acc.length; i++) {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
-
 }
 
-function onDocumentReady(callback) {
-  if (document.readyState === "complete" || document.readyState === "interactive") {
-    setTimeout(callback, 1);
-  } else {
-    document.addEventListener("DOMContentLoaded", callback); // Execute function when DOM is fully loaded
-  }
-}
-
-onDocumentReady(function () {
-  // Button click handler to toggle the 'terms' section visibility
-  const toggleBtn = document.getElementById("toggle-btn");
-  if (toggleBtn) {
-    toggleBtn.addEventListener('click', toggleTermsSection);
-  } else {
-    console.error("Button not found: #toggle-btn");
-  }
-
-  function toggleTermsSection() {
-    const termsSection = document.getElementById("terms");
-
-    for(j=0;j<acc.length;j++){
-      acc[j].classList.remove("active");
-      var panel = acc[j].nextElementSibling;
-        if (panel.style.maxHeight) {
-          panel.style.maxHeight = null;
-        }
-    }
-
-    if (termsSection.classList.contains("active")) {
-      termsSection.classList.remove("transition");
-      setTimeout(() => termsSection.classList.remove("active"), 250);
-    } else {
-      termsSection.classList.add("active");
-      setTimeout(() => termsSection.classList.add("transition"), 1);
-    }
-  }
-
-  // Section links and content to unblur
-  const sectionsData = [
-    { link: 'a[href="#abstract"]', title: 'abstract-title', text: 'abstract-text' },
-    { link: 'a[href="#intro"]', title: 'intro-title', text: 'intro-text' },
-    { link: 'a[href="#chapter-1"]', title: 'chapter-1-title', text: 'chapter-1-text' },
-    { link: 'a[href="#chapter-2"]', title: 'chapter-2-title', text: 'chapter-2-text' },
-    { link: 'a[href="#chapter-3"]', title: 'chapter-3-title', text: 'chapter-3-text' },
-    { link: 'a[href="#chapter-4"]', title: 'chapter-4-title', text: 'chapter-4-text' },
-    { link: 'a[href="#epilogue"]', title: 'epilogue-title', text: 'epilogue-text' },
-  ];
-
-  // Unblur function for sections
-  function unBlurSection(event, titleElement, textElement) {
-    event.preventDefault();
-    titleElement.style.filter = "none";
-    textElement.style.filter = "none";
-    titleElement.scrollIntoView({ behavior: "smooth" });
-  }
-
-  // Add event listeners to unblur sections on click
-  sectionsData.forEach((section) => {
-    const sectionLink = document.querySelector(section.link);
-    const sectionTitle = document.getElementById(section.title);
-    const sectionText = document.getElementById(section.text);
-
-    sectionLink.addEventListener("click", function (event) {
-      unBlurSection(event, sectionTitle, sectionText);
-    });
-  });
-
-  // Handle active navigation link highlight based on scroll position
-  const sections = document.querySelectorAll("main > div[id]");
-  const navLinks = document.querySelectorAll(".index a");
-  const circle = document.querySelector(".circle");
-
-  function updateActiveLink() {
-    let index = sections.length;
-
-    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
-
-    navLinks.forEach((link) => link.classList.remove("active"));
-    if (navLinks[index]) {
-      navLinks[index].classList.add("active");
-      const activeLink = navLinks[index];
-      const linkRect = activeLink.getBoundingClientRect();
-      const circleTop = linkRect.top + window.scrollY;
-      const circleLeft = linkRect.left + window.scrollY;
-
-      // Adjust circle position based on window width
-      if (window.innerWidth <= 768) {
-        circle.style.left = `${circleLeft}px`;
-      } else {
-        circle.style.top = `${circleTop}px`;
-      }
-    }
-  }
-
-  // Initial active link setup and scroll listener
-  updateActiveLink();
-  window.addEventListener("scroll", updateActiveLink);
-
-  // Handle click on navigation links to update circle position
-  navLinks.forEach((link) => {
-    link.addEventListener("click", function () {
-      navLinks.forEach((link) => link.classList.remove("active"));
-      this.classList.add("active");
-
-      const linkRect = this.getBoundingClientRect();
-      const circleTop = linkRect.top + window.scrollY;
-      circle.style.top = `${circleTop}px`;
-    });
-  });
-
-  // Handle window resize and adjust the circle position
-  window.addEventListener("resize", function () {
-    document.body.classList.add("resize-animation-stopper");
-    const linkRect = navLinks[navLinks.length - 1].getBoundingClientRect();
-    const circleTop = linkRect.top + window.scrollY;
-    circle.style.top = `${circleTop}px`;
-
-    setTimeout(() => {
-      document.body.classList.remove("resize-animation-stopper");
-    }, 250);
-  });
-
-  // Observing sections to remove blur when they are in view
-  observeUnblurOnView(sectionsData);
-});
-
-// Function to unblur elements when they come into view
-function observeUnblurOnView(targets) {
-  const options = { rootMargin: "0px 0px -70%", threshold: 0 };
-
-  const intersectionCallback = (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.style.filter = "none";
-      } else {
-        entry.target.removeAttribute("style");
-      }
-    });
-  };
-
-  const observer = new IntersectionObserver(intersectionCallback, options);
-
-  document.querySelectorAll('section.chapter h2, section.chapter p, section.chapter figure, section.chapter figcaption')
-    .forEach((target) => observer.observe(target));
-}
-
-
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   console.log(window.scrollY);
 });
